43b9cd9c5758c8784ba95e0423b4aa218a84777f
rbraun15 <104518622+rbraun15@users.noreply.github.com>
2024-02-02 13:34:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,8 +72,8 @@ streamlit.text ('What fruit would you like to add?')
  
 # Let's put a pick list here so they can pick the fruit they want to include
 
-add_my_fruit = streamlit.text_input2('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', add_my_fruit)
+add_my_fruit2 = streamlit.text_input2('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', add_my_fruit2)
 
 
 
